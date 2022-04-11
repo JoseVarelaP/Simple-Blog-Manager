13a54fd55d72f0a954e0148f3520c4ab3b8f0f55
Jose_Varela <jo.lu6853@gmail.com>
2022-04-11 14:25:30 -0500

    Optimization considerations from standard

--- js/functions.js
+++ js/functions.js
@@ -4,117 +4,107 @@
  * - The links to include as a clickable subpage area.
  * @param {object} Links The list of links to display.
  */
- const GenerateTopArea = ( Links ) =>
- {
-	 const doc = document.getElementById("TopAreaLinks")
-	//  const logoimg = document.createElement("img")
-	//  logoimg.src = "/img/Logo.png"
-	 
-	 const logohref = document.createElement("a")
-	 logohref.id = "logo"
-	 logohref.href = "/"
- 
-	//  logohref.appendChild(logoimg)
-	 doc.appendChild(logohref)
+const GenerateTopArea = (Links) => {
+	const doc = document.getElementById('TopAreaLinks')
 
-	 for( const L in Links )
-	 {
-		 const p = document.createElement("a")
-		 p.textContent = L
-		 p.href = `${Links[L]}/index.html`
-		 doc.appendChild(p)
-	 }
+	const logohref = document.createElement('a')
+	logohref.id = 'logo'
+	logohref.href = '/'
+
+	doc.appendChild(logohref)
+
+	for (const L in Links) {
+		const p = document.createElement('a')
+		p.textContent = L
+		p.href = `${Links[L]}/index.html`
+		doc.appendChild(p)
+	}
 }
 
 /**
- * 
+ *
  * @returns A table with key-named variables to use to parse.
  */
- function getUrlVars() {
-    let vars = {};
-    let parts = window.location.href.replace(/[?&]+([^=&]+)=([^#&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
+function getUrlVars () {
+	const vars = {}
+	const parts = window.location.href.replace(/[?&]+([^=&]+)=([^#&]*)/gi, function (m, key, value) {
+		vars[key] = value
+	})
+	return vars
 }
 
 /**
  * Generates the top menu items like About, Current Works and other subpages.
  */
-const GenSharedTopItems = () =>
-{
-	const PagePath = window.location.pathname.substring(1, window.location.pathname.lastIndexOf("/") )
+const GenSharedTopItems = () => {
+	const PagePath = window.location.pathname.substring(1, window.location.pathname.lastIndexOf('/'))
 	const Links = {
-		"Link container for other pages!": "#",
-		"Some other link here!": "#",
+		'Link container for other pages!': '#',
+		'Some other link here!': '#'
 	}
- 
-	if( PagePath.length > 1 )
-		for( const L in Links )
-		{
-			if(PagePath === Links[L])
-				Links[L] = "#"
+
+	if (PagePath.length > 1) {
+		for (const L in Links) {
+			if (PagePath === Links[L]) { Links[L] = '#' }
 		}
-	GenerateTopArea( Links )
+	}
+	GenerateTopArea(Links)
 }
 
-const CreateBlogListing = () =>
-{
-	let requestURL = './db.json'
-	let request = new XMLHttpRequest();
-	request.open('GET',requestURL);
-	request.responseType = 'json';
-	request.send();
-	request.onload = function() {
-		if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-			const data = request.response;
-			let cm = document.getElementById("draw")
-			console.log( data )
-			
-			for( const entry in data )
-			{
+const CreateBlogListing = () => {
+	const requestURL = './db.json'
+	const request = new XMLHttpRequest()
+	request.open('GET', requestURL)
+	request.responseType = 'json'
+	request.send()
+	request.onload = function () {
+		if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+			const data = request.response
+			const cm = document.getElementById('draw')
+			console.log(data)
+
+			for (const entry in data) {
 				const item = data[entry]
-				let flexcon = document.createElement("div");
-				flexcon.className = 'BlogList';
-	
-				let a = document.createElement("a")
-				a.innerHTML = `${item.Title || "title?"}`
-				a.href = `./read.html?ID=${ entry }`
-	
-				let b = document.createElement("text")
-				b.textContent = `${item.Date || "date?"}`
-	
+				const flexcon = document.createElement('div')
+				flexcon.className = 'BlogList'
+
+				const a = document.createElement('a')
+				a.innerHTML = `${item.Title || 'title?'}`
+				a.href = `./read.html?ID=${entry}`
+
+				const b = document.createElement('text')
+				b.textContent = `${item.Date || 'date?'}`
+
 				flexcon.appendChild(a)
 				flexcon.appendChild(b)
 				cm.appendChild(flexcon)
-				cm.appendChild( document.createElement("hr") )
+				cm.appendChild(document.createElement('hr'))
 			}
-			request.send();
+			request.send()
 		}
 	}
 }
 
-const CreateBlogEntry = () =>
-{
-	let l = getUrlVars();
-	var client = new XMLHttpRequest();
-	client.open('GET', `./Entries/${l.ID}.md`);
-	client.responseType = "text";
-	client.onreadystatechange = function() {
-		if(client.readyState === XMLHttpRequest.DONE) {
-			var status = client.status;
-			//<img id='backbutton' src='lar.svg'>
-			let backbutton = "<a href='index.html' alt='Return to blog listing'><img id='backbutton' src='lar.svg'></a>"
+const CreateBlogEntry = () => {
+	const l = getUrlVars()
+	const client = new XMLHttpRequest()
+	client.open('GET', `./Entries/${l.ID}.md`)
+	client.responseType = 'text'
+	client.onreadystatechange = function () {
+		if (client.readyState === XMLHttpRequest.DONE) {
+			const status = client.status
+			// <img id='backbutton' src='lar.svg'>
+			const backbutton = "<a href='index.html' alt='Return to blog listing'><img id='backbutton' src='lar.svg'></a>"
 			if (status === 0 || (status >= 200 && status < 400)) {
 				// Begin dealing with YAML information headers.
-				let cached_lex = marked.Lexer.lex
+				const cached_lex = marked.Lexer.lex
 				let headerinfo
 				marked.Lexer.lex = function (text, options) {
-					let parsed = yamlFront.loadFront(text)
-					let cKey = '__content'
-					let table = {}
-					let coini = 0
-					for (let k in parsed) {
+					const parsed = yamlFront.loadFront(text)
+					const cKey = '__content'
+					const table = {}
+					const coini = 0
+					for (const k in parsed) {
 						if (k === cKey) {
 							continue
 						}
@@ -126,52 +116,48 @@ const CreateBlogEntry = () =>
 				}
 
 				// @param object
-				const ResponseText = marked(client.responseText);
+				const ResponseText = marked(client.responseText)
 
-				const BlogTitle = `<h1>${headerinfo["Title"] || "title?"}</h1>`
-				const PostDate = `<div class="timedate">${headerinfo["Date"] || "date?"}</div>`
-				document.getElementById('text-contents').innerHTML = backbutton + BlogTitle + PostDate + ResponseText;
-				document.title = `${headerinfo["Title"] || "title?"}`
+				const BlogTitle = `<h1>${headerinfo.Title || 'title?'}</h1>`
+				const PostDate = `<div class="timedate">${headerinfo.Date || 'date?'}</div>`
+				document.getElementById('text-contents').innerHTML = backbutton + BlogTitle + PostDate + ResponseText
+				document.title = `${headerinfo.Title || 'title?'}`
 
 				const url = window.location.href
-				if( headerinfo["UseTableOfContents"] )
-				{
-					let cont = document.getElementsByClassName("TOC-container")
-					
-					if( cont.length == 0 )
-					{
-						console.warn("[UseTableOfContents] Cannot be used. TOC-container is not defined on the blog.")
+				if (headerinfo.UseTableOfContents) {
+					const cont = document.getElementsByClassName('TOC-container')
+
+					if (cont.length == 0) {
+						console.warn('[UseTableOfContents] Cannot be used. TOC-container is not defined on the blog.')
 						return	// Stop operation, as the container is not available.
 					}
 
-					const titles = document.getElementById("text-contents").getElementsByTagName("h2");
+					const titles = document.getElementById('text-contents').getElementsByTagName('h2')
 
 					console.log(titles)
-					
+
 					// Generate the table of contents.
-					const TOC = document.createElement("details")
-					const summ = document.createElement("summary")
-					summ.innerHTML = "Table of Contents"
-					TOC.appendChild( summ )
-					
+					const TOC = document.createElement('details')
+					const summ = document.createElement('summary')
+					summ.innerHTML = 'Table of Contents'
+					TOC.appendChild(summ)
+
 					// Add all items for the content.
-					const listcont = document.createElement("ul")
-					for( const title of titles )
-					{
+					const listcont = document.createElement('ul')
+					for (const title of titles) {
 						// Add the option to put the link copy on this section.
-						title.onclick = function() {
+						title.onclick = function () {
 							let urlarea = url
-							if( url.lastIndexOf('#') !== -1 )
-								urlarea = url.substring( 0, url.lastIndexOf('#') )
+							if (url.lastIndexOf('#') !== -1) { urlarea = url.substring(0, url.lastIndexOf('#')) }
 
 							window.location = `#${title.id}`
-							navigator.clipboard.writeText( urlarea + `#${title.id}` )
+							navigator.clipboard.writeText(urlarea + `#${title.id}`)
 						}
-						const item = document.createElement("li")
-						const link = document.createElement("a")
+						const item = document.createElement('li')
+						const link = document.createElement('a')
 						link.href = `#${title.id}`
 						link.textContent = title.innerText
-						item.appendChild( link )
+						item.appendChild(link)
 						listcont.appendChild(item)
 					}
 					TOC.appendChild(listcont)
@@ -180,24 +166,22 @@ const CreateBlogEntry = () =>
 				}
 
 				// Find if the user was given an ID, and jump to that location.
-				if( url.lastIndexOf('#') !== -1 )
-				{
-					const GroupOpen = url.substring( url.lastIndexOf('#') + 1 )
-					if( GroupOpen.length > 1 )
-					{
+				if (url.lastIndexOf('#') !== -1) {
+					const GroupOpen = url.substring(url.lastIndexOf('#') + 1)
+					if (GroupOpen.length > 1) {
 						window.location = `#${GroupOpen}`
 					}
 				}
 			} else {
-				document.getElementById('text-contents').innerHTML = backbutton + marked(`No page (${l.ID}) was found...`);
+				document.getElementById('text-contents').innerHTML = backbutton + marked(`No page (${l.ID}) was found...`)
 			}
 		}
-	}
-	client.send();
+  	}
+  	client.send()
 }
- 
+
 /** The site's general scale. Needed for mobile phone calculations. */
-const siteWidth = 640;
-const scale = screen.width /siteWidth;
- 
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+const siteWidth = 640
+const scale = screen.width / siteWidth
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=' + siteWidth + ', initial-scale=' + scale + '')
